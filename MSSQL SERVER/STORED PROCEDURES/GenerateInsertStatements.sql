31e3d8621abaae56042c90595448bf84cee91e3e
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2022-09-30 16:58:30 +0300

    Create GenerateInsertStatements.sql

---
+++ MSSQL SERVER/STORED PROCEDURES/GenerateInsertStatements.sql
@@ -0,0 +1,38 @@
+CREATE PROCEDURE sp_generate_insert_scripts
+(
+    @TABLE_NAME VARCHAR(MAX),
+    @FILTER_CONDITION VARCHAR(MAX)=''
+)
+AS
+BEGIN
+
+SET NOCOUNT ON
+
+DECLARE @CSV_COLUMN VARCHAR(MAX),
+        @QUOTED_DATA VARCHAR(MAX),
+        @TEXT VARCHAR(MAX)
+
+SELECT @CSV_COLUMN=STUFF
+(
+    (
+     SELECT ',['+ NAME +']' FROM sys.all_columns 
+     WHERE OBJECT_ID=OBJECT_ID(@TABLE_NAME) AND
+     is_identity!=1 FOR XML PATH('')
+    ),1,1,''
+)
+
+SELECT @QUOTED_DATA=STUFF
+(
+    (
+     SELECT ' ISNULL(QUOTENAME('+NAME+','+QUOTENAME('''','''''')+'),'+'''NULL'''+')+'','''+'+' FROM sys.all_columns 
+     WHERE OBJECT_ID=OBJECT_ID(@TABLE_NAME) AND
+     is_identity!=1 FOR XML PATH('')
+    ),1,1,''
+)
+SELECT @TEXT='SELECT ''INSERT INTO '+@TABLE_NAME+'('+@CSV_COLUMN+')VALUES('''+'+'+SUBSTRING(@QUOTED_DATA,1,LEN(@QUOTED_DATA)-5)+'+'+''')'''+' Insert_Scripts FROM '+@TABLE_NAME + IIF(@FILTER_CONDITION='','',' WHERE ' + @FILTER_CONDITION)
+
+EXECUTE (@TEXT)
+SET NOCOUNT OFF
+
+END
+
