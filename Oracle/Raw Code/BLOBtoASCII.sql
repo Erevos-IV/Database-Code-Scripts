cb5c9fdefa9812232d79b8ef050b5fe3acbf6573
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2022-10-13 10:19:51 +0300

    Create BLOBtoASCII.sql

---
+++ Oracle/Raw Code/BLOBtoASCII.sql
@@ -0,0 +1,6 @@
+-- THIS IS HOW TO GET THE FIRST 4000 LETTERS IN A BLOB TO ASCII TEXT.
+-- IF YOU NEED MORE THAN THAT, THEN ADD ONE MORE COLUMN LIKE BELOW.
+
+SELECT (utl_raw.cast_to_VARCHAR2(dbms_lob.substr(Column_Name), 2000, 1)) ||    -- Column NAME, NUMBER, position
+	      utl_raw.cast_to_VARCHAR2(dbms_lob.substr(Column_Name), 2000, 2001)))        
+from (TABLE_NAME)
