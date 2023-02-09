0e052939c49917478058a61fcc843aa9a5d50a27
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2023-02-09 14:32:18 +0200

    Update Page Lock.sql

--- MSSQL SERVER/Indexes/Page Lock.sql
+++ MSSQL SERVER/Indexes/Page Lock.sql
@@ -4,23 +4,20 @@ SELECT
     --dm_db_index_usage_stats.user_seeks,
     --dm_db_index_usage_stats.user_scans,
     --dm_db_index_usage_stats.user_updates,
-	indexes.type_desc,
-	indexes.type ,
-	indexes.is_disabled,
-	indexes.allow_page_locks,
+    indexes.type_desc,
+    indexes.type ,
+    indexes.is_disabled,
+    indexes.allow_page_locks,
 	--*
-	'ALTER INDEX [' + indexes.name +'] ON [dbo].[' + objects.name + '] SET ( ALLOW_PAGE_LOCKS = OFF )' as SCRIPT
+    'ALTER INDEX [' + indexes.name +'] ON [dbo].[' + objects.name + '] SET ( ALLOW_PAGE_LOCKS = OFF )' as SCRIPT
 FROM
     sys.dm_db_index_usage_stats
     INNER JOIN sys.objects ON dm_db_index_usage_stats.OBJECT_ID = objects.OBJECT_ID
     INNER JOIN sys.indexes ON indexes.index_id = dm_db_index_usage_stats.index_id AND dm_db_index_usage_stats.OBJECT_ID = indexes.OBJECT_ID
-WHERE
-    --objects.name = 'PROFITS_ACCOUNT'
-	indexes.allow_page_locks = 1 and indexes.type <> 0
-ORDER BY
-    --dm_db_index_usage_stats.user_updates DESC
-	objects.name
-	go
+WHERE indexes.allow_page_locks = 1 and indexes.type <> 0
+ORDER BY objects.name 
+--dm_db_index_usage_stats.user_updates DESC
 
+GO
 
 
