22e97cf8d69c6c8264492c2549e9a7656cbd08d9
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2025-08-04 12:33:21 +0300

    Create RMANViewAndReport.sql

---
+++ Oracle/BackupAndRestore/RMANViewAndReport.sql
@@ -0,0 +1,10 @@
+RMAN show all backups + pluggable dbs
+-- Connect to the instance: rman target=/
+-- list backup of database;
+
+
+RMAN Report for a specific pdb
+-- list backup of database nameofdb;
+
+RMAN show all archivelog
+-- list archivelog all
